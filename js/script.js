bb096d85f03deedbf38fdeb173af8260f6289382
asafzaf <asaf.zafrir@gmail.com>
2023-05-16 17:00:51 +0300

    Added a form of 2 stages of answering

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    $("#part2").hide();
+    $("#submit-button").hide();
+    document.getElementById("next-step-button").onclick = function() {
+        $("#back-button").prop( "disabled", false );
+        $("#process-bar").css("width", "100%");
+        $("#process-bar").text("stage 2");
+        $("#part1").hide();
+        $("#part2").show();
+        $("#next-step-button").hide();
+        $("#submit-button").show();
+    };
+
+    document.getElementById("back-button").onclick = function() {
+        $("#back-button").prop( "disabled", true );
+        $("#process-bar").css("width", "50%");
+        $("#process-bar").text("stage 1");
+        $("#part1").show();
+        $("#part2").hide();
+        $("#next-step-button").show();
+        $("#submit-button").hide();
+    };
+};
