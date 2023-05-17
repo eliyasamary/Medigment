a7bd7e958be6bc1f3da829d3c6a0d9f3769a18a9
Eliya Samary <106588315+eliyasamary@users.noreply.github.com>
2023-05-17 17:07:26 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,28 +1,24 @@
 window.onload = () => {
     $("#part2").hide();
-    $("#submit-button").hide();
+    $("#finish-button").hide();
+    $("#back-button").hide();
     document.getElementById("next-step-button").onclick = function() {
-        $("#back-button").prop( "disabled", false );
+        $("#back-button").show();
         $("#process-bar").css("width", "100%");
         $("#process-bar").text("stage 2");
         $("#part1").hide();
         $("#part2").show();
         $("#next-step-button").hide();
-        $("#submit-button").show();
+        $("#finish-button").show();
     };
 
     document.getElementById("back-button").onclick = function() {
-        $("#back-button").prop( "disabled", true );
+        $("#back-button").hide();
         $("#process-bar").css("width", "50%");
         $("#process-bar").text("stage 1");
         $("#part1").show();
         $("#part2").hide();
         $("#next-step-button").show();
-        $("#submit-button").hide();
-    };
-
-    document.getElementById("addSensitivity").onclick = function() {
-      const sensitiveBox = document.createElement("div");
-      sensitiveBox.innerText = "";  
+        $("#finish-button").hide();
     };
 };
