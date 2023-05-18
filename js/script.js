23b17c77a91696990c83d51cf38624e845f7bfc2
asafzaf <asaf.zafrir@gmail.com>
2023-05-18 09:24:12 +0300

    delete inner html of form

--- js/script.js
+++ js/script.js
@@ -22,18 +22,6 @@ window.onload = () => {
         $("#finish-button").hide();
     };
 
-    // let sensNum = 1;
-    
-    document.getElementById("add-section-btn").onclick = function() {
-        // sensNum++;
-        const pNode = document.getElementById("add-sensitive-section");
-        pNode.innerHTML += '<div class="col-md-4"><select class="form-select" name="Sensitivity1"><option selected>Select Sensitivity</option><option value="1">Anaphylaxis</option><option value="2">Sinusitis</option><option value="3">Urticaria & Angioedema</option><option value="4">Rhinitis</option><option value="5">Asthma</option><option value="6">Autoimmunity-Immune Deficiency</option><option value="7">Drug Allergy</option><option value="8">Else...</option></select></div><div class="col-md-4"><input type="text" class="form-control" placeholder="Add Comment" id="inputSensitivity"></div><div class="col-md-4"></div>';
-    };
-
-    document.getElementById("add-medication-btn").onclick = function() {
-        
-    };
-
     document.getElementById("cancel-button").onclick = function() {
         window.location.href = 'http://se.shenkar.ac.il/students/2022-2023/web1/dev_204/index.html';
     };
